f7815e520d867cec41a49f3c34a94bdeee26caba
tslearn <tslearn@163.com>
2025-08-30 04:44:43 +0800

    change

--- builder/utils/error_test.go
+++ builder/utils/error_test.go
@@ -11,13 +11,13 @@ func TestFirstError(t *testing.T) {
 		assert(FirstError(nil, nil, nil)).Equals(nil)
 	})
 
-	t.Run("first error is not nil", func(t *testing.T) {
+	t.Run("first error is nil", func(t *testing.T) {
 		assert := NewAssert(t)
-		assert(FirstError(nil, Errorf("error"), nil)).Equals(Errorf("error"))
+		assert(FirstError(nil, Errorf("error2"), nil)).Equals(Errorf("error2"))
 	})
 
-	t.Run("first error is  nil", func(t *testing.T) {
+	t.Run("first error is not nil", func(t *testing.T) {
 		assert := NewAssert(t)
-		assert(FirstError(Errorf("error1"), nil, Errorf("error2"))).Equals(Errorf("error1"))
+		assert(FirstError(Errorf("error1"), nil, Errorf("error3"))).Equals(Errorf("error1"))
 	})
 }
